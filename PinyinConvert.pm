5a10f6980582f86ea08d6d991cf4514e6b483e74
☼ 林永忠 ☼ <xern@cpan.org>
2002-11-24 12:04:57 +0000

    Import of XERN/Lingua-ZH-PinyinConvert-0.03 from CPAN.
    gitpan-cpan-distribution: Lingua-ZH-PinyinConvert
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         XERN/Lingua-ZH-PinyinConvert-0.03.tar.gz
    gitpan-cpan-author:       XERN
    gitpan-cpan-maturity:     released

--- PinyinConvert.pm
+++ PinyinConvert.pm
@@ -1,53 +1,18 @@
 package Lingua::ZH::PinyinConvert;
 
-use 5.006;
+use 5.005;
 use strict;
-use warnings;
 use Carp;
 
 require Exporter;
-require DynaLoader;
-use AutoLoader;
 
-our @ISA = qw(Exporter DynaLoader);
+use vars qw(@ISA @EXPORT_OK $VERSION);
 
-our @EXPORT_OK = qw/convert/;
+@ISA = qw(Exporter);
 
-our $VERSION = '0.01';
+@EXPORT_OK = qw/convert/;
 
-sub AUTOLOAD {
-    # This AUTOLOAD is used to 'autoload' constants from the constant()
-    # XS function.  If a constant is not found then control is passed
-    # to the AUTOLOAD in AutoLoader.
-
-    my $constname;
-    our $AUTOLOAD;
-    ($constname = $AUTOLOAD) =~ s/.*:://;
-    croak "& not defined" if $constname eq 'constant';
-    my $val = constant($constname, @_ ? $_[0] : 0);
-    if ($! != 0) {
-	if ($! =~ /Invalid/ || $!{EINVAL}) {
-	    $AutoLoader::AUTOLOAD = $AUTOLOAD;
-	    goto &AutoLoader::AUTOLOAD;
-	}
-	else {
-	    croak "Your vendor has not defined Lingua::ZH::PinyinConvert macro $constname";
-	}
-    }
-    {
-	no strict 'refs';
-	# Fixed between 5.005_53 and 5.005_61
-	if ($] >= 5.00561) {
-	    *$AUTOLOAD = sub () { $val };
-	}
-	else {
-	    *$AUTOLOAD = sub { $val };
-	}
-    }
-    goto &$AUTOLOAD;
-}
-
-bootstrap Lingua::ZH::PinyinConvert $VERSION;
+$VERSION = '0.03';
 
 my @PS = ( # pinyin systems
        [ qw/a a a a a/],
